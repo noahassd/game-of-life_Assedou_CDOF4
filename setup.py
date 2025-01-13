69f36a4c66e743f503a873c4322b78f3b4791ec7
noahASD <37837626+Nh161103@users.noreply.github.com>
2025-01-13 17:13:42 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(
- name='{name-of-your-project}',
+ name='{game-of-life_Assedou_CDOF4}',
  version='1.0',
- author='{your-full-name-or-your-github-username}',
+ author='{noahassd}',
  license='MIT',
  long_description=open('README.md').read(),
 )
